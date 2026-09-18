4a0e9173e92888180143f54c891e64905f8c7fde
hamza <=>
2026-09-18 21:08:16 +0500

    a

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(apiLimiter)
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000",],
+  origin: [ process.env.FRONTEND_URL || "http://localhost:3000",],
   credentials: true
 }))
 
