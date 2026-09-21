aeed03d278a44558b978ea57ffbbfb1d9f508240
hamza <=>
2026-09-21 21:01:09 +0500

    fix

--- src/Services/Token.Service.ts
+++ src/Services/Token.Service.ts
@@ -32,7 +32,7 @@ export const generateRefreshToken = function (payload: TokenPayLoad) {
 export const options: CookieOptions = {
     httpOnly: true,
     sameSite: "none",
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 
 }
