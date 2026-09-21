428acef4ee50f56930a5252f13f8856d82b809f3
hamza <=>
2026-09-21 21:32:18 +0500

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -45,22 +45,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow requests without an Origin header
-      // Example: Postman, server-to-server requests
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-
-      return callback(
-        new Error(`CORS blocked origin: ${origin}`)
-      );
-    },
+    origin: allowedOrigins,
 
     credentials: true,
 
